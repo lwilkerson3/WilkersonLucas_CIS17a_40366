50cc17130d60162b97fd61bfb31856c2db7fdbcf
lucasdw99 <lucasdw99@gmail.com>
2020-02-24 16:06:36 -0800

    IntroLab

---
+++ Lab/IntroLab/HelloWorld/main.cpp
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   main.cpp
+ * Author: Lucas
+ *
+ * Created on February 24, 2020, 3:52 PM
+ */
+
+#include <cstdlib>
+#include <iostream>
+
+using namespace std;
+
+/*
+ * 
+ */
+int main() {
+    cout << "Hello World!";
+    return 0;
+}
+
